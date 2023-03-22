0e49b05bdc6b90255f901e33c26cdf046e0ed24a
dchung117 <dchung0330gmail.com>
2023-03-22 02:46:35 +0000

    added matplotlib to dependencies. modified MCTS for alphazero with policy and value networks. tested in human vs computer game.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import matplotlib.pyplot as plt
+import numpy as np
 import torch
 torch.manual_seed(0)
 
 from games.tictactoe import TicTacToe
+from mcts import MCTS
 from model import ResNet
 
 if __name__ == "__main__":
@@ -11,38 +12,57 @@ if __name__ == "__main__":
     player = 1
     board = tictactoe.get_empty_board()
 
-    # Player 1/2 take actions
-    board = tictactoe.apply_move(board, 4, 1)
-    board = tictactoe.apply_move(board, 2, -1)
-    print("Current board state: \n", board)
-    print()
-
-    # Encode board, save as tensor
-    board = tictactoe.encode_board(board)
-    print("Encoded board state: ")
-    print("Player moves: \n", board[0])
-    print("Opponent moves: \n", board[1])
-    print("Empty moves: \n", board[2])
-    print()
-    state = torch.tensor(board).unsqueeze(0)
-
-    # Initialize ResNet model, setup device
-    resnet = ResNet(tictactoe, n_blocks=4, n_hidden=64)
+    # Initialize ResNet model
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    resnet = resnet.to(device)
+    model = ResNet(tictactoe, 4, 64).to(device)
+    model.eval()
 
-    # Compute policy and value
-    policy_logits, value = resnet(state.to(device))
-    value = value.item()
+    # Initialize MCTS
+    kwargs = {
+        "C": 1.4,
+        "n_searches": 100
+    }
 
-    # Apply softmax to policy
-    policy_probs = torch.softmax(policy_logits, dim=1).squeeze(0).detach().cpu().numpy()
-    print("Policy: ", policy_probs)
-    print("Value: ", value)
+    mcts = MCTS(tictactoe, kwargs, model=model, device=device)
 
-    # Plot policy
-    plt.bar(range(tictactoe.n_actions), policy_probs)
-    plt.title("Output policy for Player 1")
-    plt.xlabel("Moves")
-    plt.ylabel("Probability")
-    plt.savefig("test_policy_probs.png")
+    # Play against computer
+    while True:
+        print("Current board: ")
+        for row in board:
+            print(row)
+
+        if player == 1:
+            # Get valid moves
+            valid_moves = tictactoe.get_valid_moves(board)
+            print("Valid moves: ", valid_moves)
+            action = int(input("Select move: "))
+
+            # Ask again if move not valid
+            if valid_moves[action] == 0:
+                print("Invalid action.")
+                continue
+        else:
+            # Use MCTS w/ policy and value networks
+            neutral_state = tictactoe.change_perspective(board, player) # change board to opponent pov
+            mcts_probs = mcts.search(neutral_state)
+            action = np.argmax(mcts_probs) # select action w/ highest MCTS prob
+
+        # Apply action
+        board = tictactoe.apply_move(board, action, player)
+
+        # Get value check if game over
+        value, is_terminal = tictactoe.check_end_game(board, action)
+
+        # Break if terminal
+        if is_terminal:
+            print("Game over.")
+            for row in board:
+                print(row)
+            if value == 1:
+                print(f"Player {player} won")
+            else:
+                print("Draw")
+            break
+
+        # Switch player
+        player = tictactoe.get_opponent(player)
--- mcts.py
+++ mcts.py
@@ -1,20 +1,38 @@
 from __future__ import annotations
 from typing import Optional
 import numpy as np
+import torch
+from torch import nn
 
 from games.base import Game
 
 class Node(object):
     """
     Node (i.e. state) in Monte Carlo Tree Search algorithm
+
+    Args:
+        game: Game
+            Game to be played (e.g. TicTacToe)
+        kwargs: dict
+            Keyword arguments containing hyperparameters
+        state: np.ndarray
+            Game state array of node
+        parent: Optional[Node] = None
+            Parent node of current state
+        action_taken: Optional[int] = None
+            Action taken to reach this state
+        prior: int = 0
+            Prior policy probability of reaching this state
     """
     def __init__(self, game: Game, kwargs: dict, state: np.ndarray,
-        parent: Optional[Node] = None, action_taken: Optional[int] = None) -> None:
+        parent: Optional[Node] = None, action_taken: Optional[int] = None, prior: int = 0, is_alpha: bool = False) -> None:
         self.game = game
         self.kwargs = kwargs
         self.state = state
         self.parent = parent
         self.action_taken = action_taken
+        self.prior = prior
+        self.is_alpha = is_alpha
 
         # Initialize child node array
         self.children = []
@@ -50,27 +68,41 @@ class Node(object):
 
         return self.children[max_ucb_idx]
 
-    def expand(self) -> Node:
+    def expand(self, policy: Optional[np.ndarray] = None) -> Node:
         """
         Add unexlored child state of node
 
+        Args:
+            policy (Optional[np.ndarray]) = None: Policy used to select actions
         Returns:
-            Node: Unexplored child node
+            Optional[Node]: Unexplored child node (if no policy given), else returns None
         """
+        # Expand each child node if given policy
+        if policy is not None:
+            for action, prob in enumerate(policy):
+                # Expand every possible action
+                if prob > 0:
+                    new_state = self.game.apply_move(self.state.copy(), action, player=1)
+                    new_state = self.game.change_perspective(new_state, player=-1)
+                    new_child = Node(self.game, self.kwargs, new_state, parent=self, action_taken=action, prior=prob, is_alpha=True)
+                    self.children.append(new_child)
+
+                self.moves[action] = 0
+        else:
         # Sample unexplored child node
-        action = np.random.choice(np.where(self.moves == 1)[0], 1)
+            action = np.random.choice(np.where(self.moves == 1)[0], 1)
 
-        # Create new child node
-        # NOTE: expansion occurs from player 1 perspective -> game board is flipped when the new child state is created (i.e. switch to opponent's pov)
-        new_state = self.game.apply_move(self.state.copy(), action, player=1)
-        new_state = self.game.change_perspective(new_state, player=-1)
-        new_child = Node(self.game, self.kwargs, new_state, parent=self, action_taken=action)
-        self.children.append(new_child)
+            # Create new child node
+            # NOTE: expansion occurs from player 1 perspective -> game board is flipped when the new child state is created (i.e. switch to opponent's pov)
+            new_state = self.game.apply_move(self.state.copy(), action, player=1)
+            new_state = self.game.change_perspective(new_state, player=-1)
+            new_child = Node(self.game, self.kwargs, new_state, parent=self, action_taken=action)
+            self.children.append(new_child)
 
-        # Update expandable moves - action is no longer expandable
-        self.moves[action] = 0
+            # Update expandable moves - action is no longer expandable
+            self.moves[action] = 0
 
-        return new_child
+            return new_child
 
     def simulate(self) -> float:
         """
@@ -130,24 +162,50 @@ class Node(object):
             self.parent.backprop(value)
 
     def get_ucb(self, node: Node) -> float:
+        """
+        Compute upper confidence bound of child node during selection.
+
+        Args:
+            node: Node
+                Child node in MCTS
+        """
         # Get Q-value of child - scale between 0 and 1
         # NOTE: child nodes are states for OPPOSITE PLAYER -> want states w/ NEGATIVE STATE
-        ucb = 1 - (node.value_sum / node.visit_ct + 1)/2
+        if self.is_alpha and (node.visit_ct) == 0:
+            q_val = 0
+        else:
+            q_val = 1 - ((node.value_sum / node.visit_ct) + 1)/2
 
-        # Compute upper bound
-        if self.visit_ct > 1:
-            ucb += self.kwargs["C"]*np.sqrt(self.visit_ct/node.visit_ct)
+        # Compute upper limit
+        if self.is_alpha: # MCTS w/ policy network
+            upper_limit = self.kwargs["C"]*np.sqrt(self.visit_ct/(node.visit_ct + 1))*self.prior
+        else: # standard MCTS
+            upper_limit = self.kwargs["C"]*np.sqrt(np.log(self.visit_ct)/node.visit_ct)
+        ucb = q_val + upper_limit
 
         return ucb
 
 class MCTS(object):
     """
     Monte Carlo Tree Search (MCTS) implementation for Alpha Zero lite.
+
+    Args:
+        game: Game
+            Game to be played (e.g. TicTacToe)
+        kwargs: dict
+            Keyword arguments containing hyperparameters
+        model: Optional[nn.Module] = None
+            Optional policy and value networks
+        device: torch.device = torch.device('cpu')
+            Torch device object where model is stored
     """
-    def __init__(self, game: Game, kwargs: dict) -> None:
+    def __init__(self, game: Game, kwargs: dict, model: Optional[nn.Module] = None, device: torch.device = torch.device("cpu")) -> None:
         self.game = game
         self.kwargs = kwargs
+        self.model = model
+        self.device = device
 
+    @torch.no_grad()
     def search(self, state: np.ndarray) -> np.ndarray:
         """
         Apply MCTS starting from a specified game state.
@@ -159,10 +217,13 @@ class MCTS(object):
             np.ndarray: Policy for current state after MCTS
         """
         # Create root node
-        root = Node(self.game, self.kwargs, state)
+        if self.model != None:
+            root = Node(self.game, self.kwargs, state, is_alpha=True)
+        else:
+            root = Node(self.game, self.kwargs, state)
 
         # Run searches
-        for i_search in range(self.kwargs["n_searches"]):
+        for _ in range(self.kwargs["n_searches"]):
             curr_node = root
 
             # select leaf node - find a node whose actions are not fully explored
@@ -174,11 +235,33 @@ class MCTS(object):
             val, is_terminal = self.game.check_end_game(curr_node.state, curr_node.action_taken)
             val = self.game.get_opponent_value(val)
 
-            # expand leaf node - take an action
-            # simulate until terminal node reached
             if not is_terminal:
-                curr_node = curr_node.expand()
-                val = curr_node.simulate()
+                policy = None
+                if self.model != None: # get policy and value from model
+                    enc_state = torch.tensor(self.game.encode_board(curr_node.state), dtype=torch.float32).unsqueeze(0).to(self.device)
+                    with torch.inference_mode():
+                        policy, val = self.model(enc_state)
+
+                        # softmax on policy
+                        policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
+
+                    # set invalid moves to 0, rescale
+                    valid_moves = self.game.get_valid_moves(curr_node.state)
+                    policy *= valid_moves
+                    policy /= policy.sum()
+
+                    # get value
+                    val = val.item()
+
+                    # expand leaf node - take an action
+                    curr_node.expand(policy)
+
+                else:
+                    # expand leaf node, take random action
+                    curr_node = curr_node.expand()
+
+                    # simulate by taking random actions
+                    val = curr_node.simulate()
 
             # backpropagate node value up tree
             curr_node.backprop(val)
